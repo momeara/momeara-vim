c430c781097c9dc5319a9d13dd97fce4a584a69f
Matthew O'Meara <mattjomeara@gmail.com>
2015-06-27 20:47:01 -0400

    Update init.el
    Start using use-package to initialize new packages with having to download them

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,62 @@
+;;; Matt's Emacs init.el ;;;
+; Install packages
+;   ESS
+;   magit
+;   helm
+
+; Settings
+; key bindings
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+(if (not (package-installed-p 'use-package))
+	(progn
+		(package-refresh-contents)
+      (package-install 'use-package)))
+(require 'use-package)
+
+(use-package ess-site
+	:ensure t
+	:config
+	(ess-toggle-underscore nil)
+	(setq ess-fancy-comments nil))
+
+(use-package helm
+	:ensure t)
+	
+(use-package magit
+	:ensure t)
+	
+(use-package comint
+	:config
+	(setq comint-scroll-to-bottom-on-input t)
+	(setq comint-scroll-to-bottom-on-output t)
+	(setq comint-move-point-for-output t))
+
+; when two buffers are opened with the same name, they have the first
+; unique subdirectory appeneded to the end of the buffer name to help
+; distinguish the files
+(use-package uniquify
+	:config
+	(setq uniquify-buffer-name-style 'reverse)
+	(setq uniquify-separator "/")
+	(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified         
+	(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers 
+
+; ido mode helps quickly open files, switch buffers etc.
+; remember:
+;   C-f -> go back to normal find-file
+;   C-d -> open dired
+;   C-j -> create new file (becuase enter would open the closest matching file)
+(use-package ido
+	:config
+	(setq ido-enable-flex-matching t)
+	(ido-mode t))
 
 
 ; Enable syntax hilighting
 (global-font-lock-mode 1)
-
-
 (setq inhibit-splash-screen t)
 
 ; short cut keys for compiling
@@ -14,71 +67,7 @@
 (global-set-key [f12]         'dabbrev-expand)
 (define-key esc-map [f12]     'dabbrev-completion)
 
-
-; ESS is the mode for working with R files
-(setq load-path (cons "/home/momeara/opt/ESS/lisp" load-path))
-
-(add-to-list 'load-path "~/.emacs.d")
-
-(require 'ess-site)
-; don't have have underscores automatically converted into "<-"
-(ess-toggle-underscore nil)
-(setq ess-fancy-comments nil)
-
-; In ESS with C-c C-r scroll buffer to the end of the input
-; http://stackoverflow.com/questions/2710442/in-ess-emacs-how-can-i-get-the-r-process-buffer-to-scroll-to-the-bottom-after-a
-;(setq comint-scroll-to-bottom-on-input t)
-;(setq comint-scroll-to-bottom-on-output t)
-;(setq comint-move-point-for-output t)
-
-;ess-brace-imaginary-offset
-;; Extra indentation (over sub-block indentation) for block following
-;; an open brace which follows on the same line as a statement.
-
-;ess-brace-offset
-;; Extra indentation for braces, compared with other text in same context.
-
-;ess-continued-statement-offset
-;; Extra indent for lines not starting new statements.
-
-;ess-continued-brace-offset
-;; Extra indent for substatements that start with open-braces. This is
-;; in addition to ess-continued-statement-offset.
-
-;ess-arg-function-offset
-;; Extra indent for arguments of function foo when it is called as the
-;; value of an argument to another function in arg=foo(...) form. If
-;; not number, the statements are ;;indented at open-parenthesis
-;; following foo.
-
-;ess-expression-offset
-;; Extra indent for internal substatements of the call to expression()
-;; specified in obj <- expression(...)  form. If not a number, the
-;; statements are indented at open-parenthesis following `expression'.
-
-;ess-else-offset
-;; Extra indentation of the else clause with respect to the corresponding if.
-
-
-
-
-; when two buffers are opened with the same name, they have the first
-; unique subdirectory appeneded to the end of the buffer name to help
-; distinguish the files
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'reverse)
-(setq uniquify-separator "/")
-(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified         
-(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers 
-
-; ido mode helps quickly open files, switch buffers etc.
-; remember:
-;   C-f -> go back to normal find-file
-;   C-d -> open dired
-;   C-j -> create new file (becuase enter would open the closest matching file)
-(require 'ido)
-(ido-mode t)
-(setq ido-enable-flex-matching t)
+(put 'upcase-region 'disabled nil)
 
 ; put the current function in the bottom buffer
 (which-function-mode t)
@@ -90,7 +79,6 @@
 
 ;; Your init file should contain only one such instance.
 ;; If there is more than one, they won't work right.
-;
 (custom-set-variables
  '(safe-local-variable-values (quote ((rm-trailing-spaces . t) (show-trailing-whitespace . t) (rm-trailing-spaces . t)))))
 
@@ -138,7 +126,7 @@
 ;(add-hook 'ess-mode-hook 'rosetta-ess-mode-hook)
 
 
-(put 'upcase-region 'disabled nil)
+
 
 (setq load-path (cons "/home/momeara/opt/" load-path))
 (require 'web-mode)
